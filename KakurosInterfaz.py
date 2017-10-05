0f22f0185505d550aa1b08db20d51b1b5704be04
rrsegnini <roberto.t.x@hotmail.com>
2017-10-05 01:44:32 -0600

    Update interfaz main window

--- KakurosInterfaz.py
+++ KakurosInterfaz.py
@@ -43,11 +43,46 @@ class Application(Frame):
                          [[0,7],4,2,1,0],
                          [0,1,0,0,0],
                          [0,0,0,0,0]]
+
+        kaks = [[0,[16,0],[4,0],[6,0],0,0,[4,0],[16,0],0,0,0,0,[30,0],[4,0],0,0,0,0,[23,0],[4,0]],
+                [[0,13],-1,-1,-1,0,[7,8],-1,-1,0,0,0,[0,8],-1,-1,[4,0],0,0,[0,10],-1,-1],
+                [[0,10],-1,-1,-1,[24,16],-1,-1,-1],
+                [0,[16,0],[4,0],[6,0],0,0,[4,0],[16,0],0,0,0,0,[30,0],[4,0],0,0,0,0,[23,0],[4,0]]]
+        print(len(kaks[1]))
+
+        if self.mainWindow():
+            print ("asfa")
+        #nueva = self.new_window()
+        #w = Scale(nueva, from_=10, to=20)
+        #w.pack()
+
         
-        self.createGraphicKakuro(kakuroExample)
         
-    
-    def createGraphicKakuro(self, kakuroPorDesplegar):
+        #self.createGraphicKakuro(nueva,kakuroExample)
+    def mainWindow(self):
+        Label(text="Kakuros",font=("Helvetica", 13)).pack()
+        Label(text="Creado por Roberto Rojas Segnini y Daniel Alvarado Bonilla").pack()
+        Label(text="Seleccione el tamaño del kakuro").pack()
+        
+        
+        w = Scale( from_=10, to=20, orient=HORIZONTAL)
+        w.pack()
+
+        Button(text="Generar kakuro", command=lambda: self.createKakuro(w.get())).pack()
+        
+    def new_window(self):
+        self.newWindow = Toplevel(root)
+        #self.newFrame = Frame(self.newWindow)
+        
+        return self.newWindow
+    def createKakuro(self, size):
+        newKak = crearMatriz(size)
+        nueva = self.new_window()
+        self.createGraphicKakuro(nueva,newKak) 
+        
+    def createGraphicKakuro(self,newWin, kakuroPorDesplegar):
+        newWin.config(width=len(kakuroPorDesplegar)*100, height=len(kakuroPorDesplegar)*100)
+        #newWin = self.new_window()
         photoBLACK = PhotoImage(file="BLACK.png")
         photoSLASH = PhotoImage(file="SLASH.png")
         
@@ -70,20 +105,22 @@ class Application(Frame):
             for j in range(0, len(kakuroPorDesplegar[0])):
 
                 if kakuroPorDesplegar[i][j] == 0:
-                    labelCASILLA = Label(image=photoBLACK)
+                    labelCASILLA = Label(newWin,image=photoBLACK)
                     labelCASILLA.image = photoBLACK
                     labelCASILLA.place(x=placeX, y=placeY)
                 elif isinstance(kakuroPorDesplegar[i][j], list):
-                    labelCASILLA = Label(image=photoSLASH)
+                    labelCASILLA = Label(newWin,image=photoSLASH)
                     labelCASILLA.image = photoSLASH
                     labelCASILLA.place(x=placeX, y=placeY)
                     if kakuroPorDesplegar[i][j][0] != 0:
-                        labelNUM = Label(text=str(kakuroPorDesplegar[i][j][0]), font=("Helvetica", 13), fg="white", bg="black")
+                        labelNUM = Label(newWin,text=str(kakuroPorDesplegar[i][j][0]), font=("Helvetica", 13), fg="white", bg="black")
                         labelNUM.place(x=placeX+20, y=placeY+50)
                     if kakuroPorDesplegar[i][j][1] != 0:
-                        labelNUM = Label(text=kakuroPorDesplegar[i][j][1], font=("Helvetica", 13), fg="white", bg="black")
+                        labelNUM = Label(newWin,text=kakuroPorDesplegar[i][j][1], font=("Helvetica", 13), fg="white", bg="black")
                         labelNUM.place(x=placeX+52, y=placeY+20)
+                        
                 else:
+                    
                     sizeVars = len(self.variablesEntry)
                     self.variablesEntry.append(StringVar())
                     self.variablesEntry[sizeVars].trace("w", lambda name, index, mode, var=self.variablesEntry[sizeVars], sizeVars=sizeVars:
@@ -92,28 +129,32 @@ class Application(Frame):
                     
                     #v = StringVar()
                     #v.trace("w", lambda *args:self.callback(v))
-                    self.CASILLA = Entry(width=2,font=large_font, justify=CENTER, textvariable = self.variablesEntry[sizeVars])
+                    self.CASILLA = Entry(newWin,width=2,font=large_font, justify=CENTER, textvariable = self.variablesEntry[sizeVars])
                     listaCASILLAS+=[self.CASILLA]
                     
                     self.variablesEntry[sizeVars].trace("w", lambda name, index, mode, var=self.variablesEntry[sizeVars], sizeVars=sizeVars:
                               self.entryupdate(var, sizeVars, listaCASILLAS))
                     
                     self.CASILLA.place(x=placeX, y=placeY)
+                    if kakuroPorDesplegar[i][j] != -1:
+                        #print (kakuroPorDesplegar[i][j])
+                        self.variablesEntry[-1].set(kakuroPorDesplegar[i][j])
+                        
                 placeX+=90
             placeY+=85
 
         #self.verificarButton = Button(text="Verificar solución", font=("Helvetica", 13),
                                       #command=lambda:self.verificarSolucion(listaCASILLAS,kakuroExample))
-        self.verificarButton = Button(text="Verificar solución", font=("Helvetica", 13),
+        self.verificarButton = Button(newWin,text="Verificar solución", font=("Helvetica", 13),
                                       command=lambda:self.verificarSolucion(self.variablesEntry,kakuroPorDesplegar))
         self.verificarButton.place(x=10, y=placeY+10)
         
         
-        self.resolverButton = Button(text="Resolver (backtracking)", font=("Helvetica", 13),
-                                      command=lambda:self.solucionarKakuro(self.variablesEntry,kakuroExampleSolved))
+        self.resolverButton = Button(newWin,text="Resolver (backtracking)", font=("Helvetica", 13),
+                                      command=lambda:self.solucionarKakuro(self.variablesEntry,kakuroExampleSolved, newWin))
         self.resolverButton.place(x=200, y=placeY+10)
         
-    def solucionarKakuro(self, listaCASILLAS, kakuro):
+    def solucionarKakuro(self, listaCASILLAS, kakuro, _newWin):
         listaSOLUCIONES=[]
         copiaKakuroOriginal = copy.deepcopy(kakuro)
 
@@ -123,9 +164,9 @@ class Application(Frame):
                 if kakuro[x][y] == -1:
                     listaCASILLAS[cont].set(kakuro[x][y])
                     cont+=1
-        self.createGraphicKakuro(kakuro)
-        print(copiaKakuroOriginal)
-        app.mainloop()
+        self.createGraphicKakuro(_newWin,kakuro)
+        #print(copiaKakuroOriginal)
+        
         #solveBruteForce(copiaKakuroOriginal, [0,0])
         
     def verificarSolucion(self, listaCASILLAS, kakuro):
@@ -158,7 +199,7 @@ class Application(Frame):
         
     def __init__(self, master=None):
         Frame.__init__(self, master,width=600, height=600)
-        self.pack()
+        #self.pack()
         self.createWidgets()
 
 root = Tk()
